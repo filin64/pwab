fbaf5d493c2fe3a8908a9d265672ba596fd929a7
dmitry <d.filin64@gmaul.com>
2017-05-17 19:31:35 +0300

    Initial commit

---
+++ src/pwab.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# encoding: utf8
+
+"""
+    Public Wi-Fi Autorization Bypass
+    Copyright (c)
+"""
+
+import sys
+
+def main(argv):
+    print ("Output:", argv)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
